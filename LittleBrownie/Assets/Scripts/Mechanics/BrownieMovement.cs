02220ab8356768345cbe3792bb0d85e5de37ffc6
akchurinabarbara <48187669+akchurinabarbara@users.noreply.github.com>
2023-06-30 00:56:47 +0300

    browmie animation

--- LittleBrownie/Assets/Scripts/Mechanics/BrownieMovement.cs
+++ LittleBrownie/Assets/Scripts/Mechanics/BrownieMovement.cs
@@ -1,4 +1,6 @@
+using Spine.Unity;
 using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -7,31 +9,100 @@ public class BrownieMovement : MonoBehaviour
 {
     [SerializeField]
     private float _speed;
+    [SerializeField]
+    private SkeletonAnimation _skeletonAnimation;
+    [SerializeField]
+    private float _idleDelay = 15.0f;
+
+    private List<string> _idleAnimations = new List<string> 
+    {
+        "idle",
+        "jump",
+        "levitate",
+        "think",
+        "wait",
+        "wait2"
+    };
+
+    private List<string> _moveAnimations = new List<string>
+    {
+        "run_left",
+        "run_right"
+    };
 
     private Vector2 _nextTargetPosition;
     private Coroutine _moveCoroutine;
-    private bool _isOutCameraMove = false;
+    private Coroutine _randomIdleAnimationCoroutine;
+    private bool _isOutCameraMove = false;        
 
     private CameraMovement _mainCameraMovementController;
 
     private IEnumerator Move(Vector3 target)
     {
+        _skeletonAnimation.AnimationState.ClearTrack(0);
         target.z = transform.position.z;
+
+        //Выбираем анимацию движения в сторону
+        if ((transform.position.x - target.x > 0))
+        {
+            _skeletonAnimation.AnimationState.SetAnimation(0, _moveAnimations[0], true);
+        }
+        else
+        {
+            _skeletonAnimation.AnimationState.SetAnimation(0, _moveAnimations[1], true);
+        }
 
         //Движемся, пока не достигнем цели
         while (Vector3.Distance(transform.position, target) > 0.001f)
         {
-            var step = _speed * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(transform.position, target, step);
+            var step = _speed * Time.deltaTime;            
+
+            transform.position = Vector3.MoveTowards(transform.position, target, step);            
+
             yield return new WaitForEndOfFrame();
+        }
+
+        StartPlayRandomIdle();
+    }
+
+    private void StartPlayRandomIdle()
+    {
+        _skeletonAnimation.AnimationState.SetAnimation(0, _idleAnimations[0], true);
+
+        StopPlayRandomIdle();
+        _randomIdleAnimationCoroutine = StartCoroutine(PlayRandomIdle());
+    }
+
+    private void StopPlayRandomIdle()
+    {
+        if (_randomIdleAnimationCoroutine != null)
+        {
+            StopCoroutine(_randomIdleAnimationCoroutine);
         }
     }
 
+    private IEnumerator PlayRandomIdle()
+    {
+        //ранжомная анимация
+        while (true)
+        {
+            yield return new WaitForSeconds(_idleDelay);
+            int randomAnimationIndex = Random.Range(0, _idleAnimations.Count);
+            _skeletonAnimation.AnimationState.SetAnimation(0, _idleAnimations[randomAnimationIndex], false);
+            _skeletonAnimation.AnimationState.AddAnimation(0, _idleAnimations[0], true, 0.0f);
+        }        
+    }
+ 
+
     private void StartMove(Vector2 target)
     {
+        StopPlayRandomIdle();
+
         if (_moveCoroutine != null)
         {
             StopCoroutine(_moveCoroutine);
+
+            _skeletonAnimation.AnimationState.SetAnimation(0, _idleAnimations[0], true);
         }
 
         _moveCoroutine = StartCoroutine(Move(target));
@@ -63,6 +134,11 @@ public class BrownieMovement : MonoBehaviour
         {
             StartMove(new Vector2(Camera.main.transform.position.x, transform.position.y));
         }
+    }
+
+    private void Start()
+    {
+        StartPlayRandomIdle();
     }
 
     private void OnBecameInvisible()
